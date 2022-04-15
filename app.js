49a38a7fb3f0ccfef195994c31c451b5bda898bf
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-04-15 13:11:44 -0300

    feat: add a function that takes an operator and 2 numbers as a parameter and then calls one of the 4 basic function(+, -, * or /)

--- app.js
+++ app.js
@@ -15,4 +15,26 @@ function divide(...num) {
 }
 
 
-console.log(divide(100, 50))
+
+// Creating a new function that takes a operator and 2 numbers and call any of the above functions
+const operate = (operator, n1, n2) => {
+  if (operator == '+') return add(n1, n2)
+  else if (operator == '-') return subtract(n1, n2)
+  else if (operator == '*') return multiply(n1, n2)
+  else if (operator == '/') return divide(n1, n2)
+  else return 'Please enter a valid symbol'
+}
+console.log(operate('*', 2, 5))
+
+
+
+
+// I will use this later on
+
+// function operator(...num) {
+//   return operator(num)
+// }
+// function operate(operator) {
+//   return operator
+// }
+// console.log(operate(divide(10, 5)))
